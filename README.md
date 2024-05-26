c2694f1d726bb65bba32a0dae64f5845ffbcf35f
XyrenTheCoder <95626487+XyrenTheCoder@users.noreply.github.com>
2024-05-26 20:33:46 +0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,10 +1,12 @@
+
+
     round_1
     ---------------------------------------
     Overall outcome:
         Won:
             by Resignation: 23 games
             by Checkmate: 25 games
-            on Time: 1 games
+            on Time: 6 games
             by Abandonment: 1 games
         Drawn:
             by Agreement: 2 games
@@ -12,38 +14,11 @@
             by Stalemate: 1 games
     Popular first moves:
         1.e4: 36 games
-            Continuations:
-                1...e5: 13 games
-                1...c5: 9 games
-                1...c6: 6 games
-                1...e6: 5 games
-                1...a6: 1 games
-                1...f5: 1 games
-                1...g6: 1 games
         1.d4: 10 games
-            Continuations:
-                1...Nf6: 6 games
-                1...d5: 3 games
-                1...e6: 1 games
         1.c4: 6 games
-            Continuations:
-                1...c6: 2 games
-                1...c5: 1 games
-                1...e5: 1 games
-                1...a6: 1 games
-                1...b6: 1 games
         1.Nf3: 3 games
-            Continuations:
-                1...e5: 1 games
-                1...d5: 1 games
-                1...d6: 1 games
         1.f4: 2 games
-            Continuations:
-                1...e5: 2 games
         1.g3: 2 games
-            Continuations:
-                1...c5: 1 games
-                1...d5: 1 games
         1.Nc3: 1 games
             Continuations:
                 1...e5: 1 games
@@ -55,33 +30,17 @@
         Won:
             by Checkmate: 7 games
             by Resignation: 11 games
-            on Time: 1 games
+            on Time: 2 games
             by Abandonment: 2 games
         Drawn:
             by Stalemate: 1 games
             by Repetition: 3 games
     Popular first moves:
         1.e4: 18 games
-            Continuations:
-                1...e5: 9 games
-                1...c5: 5 games
-                1...c6: 2 games
-                1...e6: 1 games
-                1...a6: 1 games
         1.d4: 4 games
-            Continuations:
-                1...Nf6: 2 games
-                1...e6: 1 games
-                1...g6: 1 games
         1.Nc3: 1 games
-            Continuations:
-                1...e5: 1 games
         1.c4: 1 games
-            Continuations:
-                1...e5: 1 games
         1.e3: 1 games
-            Continuations:
-                1...d5: 1 games
         1.Nf3: 1 games
             Continuations:
                 1...d5: 1 games
@@ -96,12 +55,22 @@
             by Repetition: 1 games
     Popular first moves:
         1.d4: 2 games
-            Continuations:
-                1...g6: 1 games
-                1...e6: 1 games
         1.e4: 2 games
             Continuations:
                 1...c6: 2 games
+
+
+    round_4
+    ---------------------------------------
+    Overall outcome:
+        Won:
+            on Time: 2 games
+        Drawn:
+    Popular first moves:
+        1.e4: 1 games
+        1.d4: 1 games
+            Continuations:
+                1...Nf6: 1 games
 
 
     losers_round_1
@@ -113,14 +82,7 @@
         Drawn:
     Popular first moves:
         1.e4: 8 games
-            Continuations:
-                1...e5: 5 games
-                1...c6: 1 games
-                1...c5: 1 games
-                1...d6: 1 games
         1.d4: 2 games
-            Continuations:
-                1...d5: 2 games
         1.a4: 1 games
             Continuations:
                 1...e5: 1 games
@@ -130,23 +92,17 @@
     ---------------------------------------
     Overall outcome:
         Won:
-            by Checkmate: 7 games
-            by Resignation: 3 games
-            on Time: 1 games
+            by Checkmate: 10 games
+            by Resignation: 4 games
+            on Time: 2 games
         Drawn:
             by Agreement: 1 games
             by Stalemate: 1 games
     Popular first moves:
-        1.e4: 10 games
+        1.e4: 12 games
+        1.d4: 6 games
             Continuations:
-                1...c6: 3 games
-                1...e5: 3 games
-                1...c5: 2 games
-                1...e6: 1 games
-                1...g6: 1 games
-        1.d4: 3 games
-            Continuations:
-                1...Nf6: 1 games
+                1...Nf6: 4 games
                 1...d5: 1 games
                 1...f5: 1 games
 
@@ -160,8 +116,6 @@
         Drawn:
     Popular first moves:
         1.f4: 1 games
-            Continuations:
-                1...d5: 1 games
         1.e4: 1 games
             Continuations:
                 1...c5: 1 games
--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from datetime import datetime
 
 # init
 just_fix_windows_console()
+global res
+res = ""
 
 
 def collect(round: str):
@@ -14,13 +16,14 @@ def collect(round: str):
 
     data = data.split('\n\n\n\n') # idk y 4 '\n's tbh
 
-    # global first_w_moves1, first_b_moves1, wintermin, drawtermin
+
     first_w_moves1 = {}
     first_b_moves1 = {}
 
     termination = []
     wintermin = {}
     drawtermin = {}
+
 
     for i in data:
         i = i.split(']\n')
@@ -31,6 +34,7 @@ def collect(round: str):
         pgn = re.sub('\n', ' ', pgn) # get rid of useless new lines that are for making pgn readable
 
         moves = pgn.split()
+
         try:
             first_w_moves1[moves[0]] += 1 # {'d4': 1}
         except:
@@ -42,6 +46,8 @@ def collect(round: str):
         except:
             first_b_moves1[moves[0]][moves[1]] = 1
 
+
+    print(termination)
     for j in termination:
         # wins
         if j.endswith('resignation"'):
@@ -49,16 +55,19 @@ def collect(round: str):
                 wintermin['by Resignation'] += 1
             except:
                 wintermin['by Resignation'] = 1
+
         elif j.endswith('checkmate"'):
             try:
                 wintermin['by Checkmate'] += 1
             except:
                 wintermin['by Checkmate'] = 1
+
         elif j.endswith('on time"'):
             try:
-                wintermin['on time'] += 1
+                wintermin['on Time'] += 1
             except:
                 wintermin['on Time'] = 1
+
         elif j.endswith('game abandoned"'):
             try:
                 wintermin['by Abandonment'] += 1
@@ -70,26 +79,31 @@ def collect(round: str):
                 drawtermin['by Agreement'] += 1
             except:
                 drawtermin['by Agreement'] = 1
+
         elif j.endswith('repetition"'):
             try:
                 drawtermin['by Repetition'] += 1
             except:
                 drawtermin['by Repetition'] = 1
+
         elif j.endswith('stalemate"'):
             try:
                 drawtermin['by Stalemate'] += 1
             except:
                 drawtermin['by Stalemate'] = 1
+
         elif j.endswith('50-move rule"'):
             try:
                 drawtermin['by 50 Move Rule'] += 1
             except:
                 drawtermin['by 50 Move Rule'] = 1
+
         elif j.endswith('insufficient material"'):
             try:
                 drawtermin['by Insufficient Material'] += 1
             except:
                 drawtermin['by Insufficient Material'] = 1
+
         elif j.endswith('by timeout vs insufficient material"'):
             try:
                 drawtermin['by Timeout VS Insufficient'] += 1
@@ -99,31 +113,31 @@ def collect(round: str):
 
 # --------------------------------------------------------
     fwm1 = dict(sorted(first_w_moves1.items(), key=lambda item: item[1], reverse=True)) # sorted by games count in descending order
+    global res
 
+    res += f"\n\n    {round}\n    ---------------------------------------\n"
+    res += "    Overall outcome:\n"
+    res += "        Won:\n"
 
-    # outputs
-    print(f"\n\n    {Style.RESET_ALL}{Fore.YELLOW}{round}\n    ---------------------------------------")
-
-    print(f"    {Style.RESET_ALL}{Fore.BLUE}Overall outcome:")
-
-    print(f"        {Fore.GREEN}Won:")
     for wins in wintermin:
-        print(f"            {Fore.GREEN}{wins}: {Fore.WHITE}{wintermin[wins]} games")
-    print(f"        {Style.DIM}Drawn:")
+        res += f"            {wins}: {wintermin[wins]} games\n"
+
+    res += "        Drawn:\n"
+
     for draws in drawtermin:
-        print(f"            {Style.DIM}{draws}: {Style.RESET_ALL}{Fore.WHITE}{drawtermin[draws]} games")
+        res += f"            {draws}: {drawtermin[draws]} games\n"
 
-
-    print(f"    {Style.RESET_ALL}{Fore.BLUE}Popular first moves:")
+    res += "    Popular first moves:\n"
 
     for i in fwm1:
-        print(f"        {Fore.YELLOW}1.{i}: {Fore.WHITE}{fwm1[i]} games")
-        print(f"            {Fore.CYAN}Continuations:")
-        for k in first_b_moves1[i]:
-            fbm1 = dict(sorted(first_b_moves1[i].items(), key=lambda item: item[1], reverse=True))
-        for j in fbm1:
-            print(f"                {Fore.MAGENTA}1...{j}: {Style.RESET_ALL}{fbm1[j]} games")
+        res += f"        1.{i}: {fwm1[i]} games\n"
 
+    res += "            Continuations:\n"
+
+    for k in first_b_moves1[i]:
+        fbm1 = dict(sorted(first_b_moves1[i].items(), key=lambda item: item[1], reverse=True))
+    for j in fbm1:
+        res += f"                1...{j}: {fbm1[j]} games\n"
 
 collect('round_1')
 collect('round_2')
@@ -132,3 +146,7 @@ collect('round_4')
 collect('losers_round_1')
 collect('losers_round_2')
 collect('losers_round_3')
+
+with open("README.md", "w") as f:
+    f.write(res)
+f.close()
